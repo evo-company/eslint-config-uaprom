4f3c690a7ec6209677f6b53bcca4007bb354c700
a.mostovenko <a.mostovenko@smartweb.com.ua>
2017-05-17 17:33:57 +0300

    upd config due to new airbnb changes. Forbid some rules.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ module.exports = {
         'no-debugger': 1,
         'max-nested-callbacks': [1, 3],
         'no-console': 1,
+        "quotes": ["error", "single", { "allowTemplateLiterals": true }],
         'indent': [2, 4, {'SwitchCase': 1}],
         'react/jsx-indent': [2, 4],
         'react/jsx-indent-props': [2, 4],
@@ -27,8 +28,34 @@ module.exports = {
         'no-param-reassign': 0,
         'no-empty-pattern': 0,
         "no-duplicate-imports": 0,
+
         "import/no-duplicates": 2,
         "import/no-commonjs": 2,
+        "import/prefer-default-export": 0,
+        "import/no-extraneous-dependencies": 0,
         "import/extensions": 0,
+
+        "comma-dangle": ["error", {
+          "arrays": "only-multiline",
+          "objects": "only-multiline",
+          "imports": "only-multiline",
+          "exports": "only-multiline",
+          "functions": "ignore"
+        }],
+
+        "no-restricted-syntax": ["error",
+          {
+            "selector": "ForInStatement",
+            "message": "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array."
+          },
+          {
+            "selector": "LabeledStatement",
+            "message": "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand."
+          },
+          {
+            "selector": "WithStatement",
+            "message": "`with` is disallowed in strict mode because it makes code impossible to predict and optimize."
+          }
+        ],
     }
 };
